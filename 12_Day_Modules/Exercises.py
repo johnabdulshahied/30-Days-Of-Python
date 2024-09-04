ae2d156e0c9d50203fb252febe9331bd1a6cb049
johnabdulshahied <70472593+johnabdulshahied@users.noreply.github.com>
2024-09-04 11:20:01 +0300

    Update Exercises.py

--- 12_Day_Modules/Exercises.py
+++ 12_Day_Modules/Exercises.py
@@ -3,6 +3,6 @@ import string
 
 def user_id_gen_by_user(n,count): # 2 - user_id_gen_by_user function
     for i in range(count):
-        print(''.join(random.choices(string.ascii_lowercase + string.digits,k=n)))
+        print(''.join(random.choices(string.ascii_letters + string.digits,k=n)))
 
 user_id_gen_by_user(n = int(input('Enter number of characters: ')),count = int(input('Enter count: ')))
